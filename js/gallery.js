ad4acec758909f8e698b5047e668de82afbe7e32
Leo Angelo Larre Agpoon <leoangelolarreagpoon@gmail.com>
2025-07-15 17:46:34 +0800

    Add blur event to image previews for improved accessibility

--- js/gallery.js
+++ js/gallery.js
@@ -6,5 +6,9 @@ function upDate(previewPic) {
 
 function unDo() {
     document.getElementById("image").style.backgroundImage = "url('')";
-    document.getElementById("image").innerHTML = "Hover over an image below to display here.";		
+    document.getElementById("image").innerHTML = "Hover over an image below to display here.";	
+    for (let i = 0; i < document.images.length; i++) {
+        document.images[i].style.filter = "blur(0px)";
+        document.images[i].setAttribute("tabindex", "0");
+    }	
 }
